000805676080f99099c29fe1252d4a01fecadb6f
pieraqua <pauloffxii@hotmail.com>
2024-03-11 00:22:04 -0300

    Coordinate transforms working, control by mouse

--- src/main/java/br/com/pieraqua/javarender3d/JavaRender3D.java
+++ src/main/java/br/com/pieraqua/javarender3d/JavaRender3D.java
@@ -10,32 +10,68 @@ import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Path2D;
 import java.util.List;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
 
 /**
  *
  * @author Paulo
+ * 
+ * Reference: https://www.alibabacloud.com/blog/construct-a-simple-3d-rendering-engine-with-java_599599
  */
 public class JavaRender3D {
 
+    double x[] = {0};
+    double y[] = {0};
     private void createAndShowGui()
     {
         JFrame frame = new JFrame();
-        frame.setSize(600,600);
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
         
+        // slider to control horizontal rotation
+        JSlider headingSlider = new JSlider(-180, 180, 0);
+        //pane.add(headingSlider, BorderLayout.SOUTH);
+
+        // slider to control vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        //pane.add(pitchSlider, BorderLayout.EAST);
+        
+        
         // panel to display render results
         JPanel renderPanel = new JPanel(){
+            
             @Override
             public void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
+                
+                double heading = Math.toRadians(x[0]);
+                //double heading = Math.toRadians(headingSlider.getValue());
+                // XZ rotation
+                Matrix3 headingTransform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, -Math.sin(heading),
+                        0, 1, 0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                    });
+                double pitch = Math.toRadians(y[0]);
+                //double pitch = Math.toRadians(pitchSlider.getValue());
+                // YZ rotation
+                Matrix3 pitchTransform = new Matrix3(new double[] {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                    });
+                // pitch and heading transform
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
                 
                 // rendering magic
                 List<Triangle> tris = new ArrayList<>();
@@ -59,10 +95,13 @@ public class JavaRender3D {
                 g2.translate(getWidth()/2, getHeight()/2);
                 g2.setColor(Color.WHITE);
                 for(Triangle t : tris){
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2.draw(path);
                 }
@@ -70,9 +109,31 @@ public class JavaRender3D {
             }
         };
         
+        
+        renderPanel.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                double yi = 180.0 / renderPanel.getHeight();
+                double xi = 180.0 / renderPanel.getWidth();
+                x[0] = (int) (e.getX() * xi);
+                y[0] = -(int) (e.getY() * yi);
+                renderPanel.repaint();
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+        
+        
         pane.add(renderPanel, BorderLayout.CENTER);
         
+        //headingSlider.addChangeListener(e -> renderPanel.repaint());
+        //pitchSlider.addChangeListener(e -> renderPanel.repaint());
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,600);
         frame.setVisible(true);
     }
     
---
+++ src/main/java/br/com/pieraqua/javarender3d/Matrix3.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.pieraqua.javarender3d;
+
+/**
+ * Class for a 3x3 matrix. 
+ * Probably best to implement a more general matrix class, but this is enough for now.
+ * 
+ * @author Paulo
+ * 
+ * Reference: https://www.alibabacloud.com/blog/construct-a-simple-3d-rendering-engine-with-java_599599
+ */
+public class Matrix3 {
+    double[] values;
+    Matrix3(double[] values) {
+        this.values = values;
+    }
+    Matrix3 multiply(Matrix3 other) {
+        double[] result = new double[9];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                for (int i = 0; i < 3; i++) {
+                    result[row * 3 + col] +=
+                        this.values[row * 3 + i] * other.values[i * 3 + col];
+                }
+            }
+        }
+        return new Matrix3(result);
+    }
+    Vertex transform(Vertex in) {
+        return new Vertex(
+            in.x * values[0] + in.y * values[3] + in.z * values[6],
+            in.x * values[1] + in.y * values[4] + in.z * values[7],
+            in.x * values[2] + in.y * values[5] + in.z * values[8]
+        );
+    }
+}
